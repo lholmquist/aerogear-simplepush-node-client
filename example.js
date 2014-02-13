78d29b8bdbdff137a90489fc5a4d11c1c5c9a6d0
Lucas Holmquist <lholmqui@redhat.com>
2014-02-13 12:58:10 -0500

    initial commit

---
+++ example.js
@@ -0,0 +1,48 @@
+var client = require('./index'),
+    mailEndpoint, mailRequest, pushClient, otherRequest, otherEndpoint;
+
+pushClient = client.SimplePushClient({
+    simplePushServerURL: 'ws://lincolnhawk:7777/simplepush/websocket',
+    onConnect: function () {
+        console.log('connected');
+
+        mailRequest = pushClient.push.register();
+
+        otherRequest = pushClient.push.register();
+
+        mailRequest.onsuccess = function (event) {
+            mailEndpoint = event;
+
+            if (mailEndpoint.pushEndpoint) {
+                console.log('push endpoint url for mail', mailEndpoint.pushEndpoint);
+            } else {
+                console.log('mail be registered');
+            }
+
+            console.log('subscribed to mail messages on' + mailEndpoint.channelID);
+        };
+
+        otherRequest.onsuccess = function (event) {
+            otherEndpoint = event;
+
+            if (otherEndpoint.pushEndpoint) {
+                console.log('push endpoint url for otherEndpoint', otherEndpoint.pushEndpoint);
+            } else {
+                console.log('otherEndpoint be registered');
+            }
+
+            console.log('subscribed to otherEndpoint messages on' + otherEndpoint.channelID);
+        };
+
+
+        pushClient.on('push', function (message) {
+            if (message.channelID === mailEndpoint.channelID) {
+                // let's react on that mail....
+                console.log('Mail Notification - ' + message.version);
+            } else {
+                console.log('Other Notification - ' + message.version);
+            }
+        });
+    },
+    onClose: function () {}
+});
